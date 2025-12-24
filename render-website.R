adcc5049ee3b947c8aa45b0500a0d1b2a60aa5e2
Jillian Reynolds <jillianwreynolds@icloud.com>
2025-12-23 16:03:57 -0800

    Updates

--- render-website.R
+++ render-website.R
@@ -2,6 +2,8 @@ last_modified_date <- format(file.info("index.qmd")$mtime, "%Y%m%d")
 
 output_file_name <- glue::glue("Reynolds-Jillian_CV-{last_modified_date}.pdf")
 
-quarto_render("index.qmd", output_format = "all", output_file = output_file_name)
+quarto_render("index.qmd", output_format = "pdf", output_file = output_file_name)
 
-quarto_preview()
+quarto_render("index.qmd", output_format = "html")
+
+quarto_preview("index.qmd")
